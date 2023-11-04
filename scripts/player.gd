f2d977105eaf905160cbb83d55d33dbc009005fe
GSM429 <gavmontross@gmail.com>
2023-11-04 17:45:26 -0400

    Created Animation Tree

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,50 +1,63 @@
 extends CharacterBody2D
 
+@export var speed : float = 100.0
+@export var jump_velocity : float = -200.0
+@export var double_jump_velocity : float = -150.0
 
-const SPEED = 100
-const JUMP_VELOCITY = -350
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var state_machine : CharacterStateMachine = $CharacterStateMachine
 
-@onready var ap = $AnimatedSprite2D
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var has_double_jumped = false
+var animation_locked = false
+var direction : Vector2 = Vector2.ZERO
+var was_in_air : bool = false
+
+func _ready():
+	animation_tree.active = true
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
+		was_in_air = true
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		has_double_jumped = false
 		
-	if direction != 0:
-		ap.flip_h = direction == -1
 		
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			jump()
+		elif not has_double_jumped:
+			double_jump()
+	
+	direction = Input.get_vector("move_left", "move_right", "jump", "down")
+	
+	if direction.x != 0 && state_machine.check_if_can_move():
+		velocity.x = direction.x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	
 	move_and_slide()
-	
-	update_animations(direction)
-	
-	_attacks()
-func update_animations(direction):
-	if is_on_floor():
-		if direction == 0:
-			ap.play('idle')
-	else:
-		if velocity.y < 0:
-			ap.play("jump")
+	update_animation()
+	update_facing_direction()
 
-func _attacks():
-	var has_attacked = false
-	if Input.is_action_just_pressed('attack'):
-		ap.play("attack1")
-		has_attacked = true
-	elif Input.is_action_just_pressed('attack')
+
+func update_animation():
+	animation_tree.set("parameters/Move/blend_position", direction.x)
 	
+func update_facing_direction():
+	if direction.x > 0:
+		sprite.flip_h = false
+	elif direction.x < 0:
+		sprite.flip_h = true
+func jump():
+	velocity.y = jump_velocity
+	animation_locked = true
+	
+func double_jump():
+	velocity.y = double_jump_velocity
+	animation_locked = true
+	has_double_jumped = true
---
+++ scripts/state.gd
@@ -0,0 +1,5 @@
+extends Node
+
+class_name state
+
+@export var can_move : bool = true
