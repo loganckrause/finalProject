3567e60f02b0bf7fd715931e66848951e81e5827
loganckrause <loganckrause@outlook.com>
2023-11-04 18:26:22 -0400

    made gun flip

--- objects/bullet.gd
+++ objects/bullet.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-var speed = 1500
+var speed = 500
 
 func _ready():
 	set_as_top_level(true)
@@ -10,11 +10,11 @@ func _process(delta):
 	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
 	
 	
-
-func _physics_process(delta):
+"""func _physics_process(delta):
 	# yield(get_tree().create_timer(0.01), "timeout")
 	$sprite.frame = 1
-	set_physics_process(false)
+	set_physics_process(false)"""
+
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free() # Replace with function body.
--- objects/gun.gd
+++ objects/gun.gd
@@ -7,13 +7,24 @@ var bullet = preload("res://objects/bullet.tscn")
 
 func _ready():
 	set_as_top_level(true)
+
+func _process(delta):
 	
+	if get_global_mouse_position().x > get_parent().global_position.x:
+		set_flip_v(false)
+		sprite.position = get_parent().position.x-10
+	else:
+		set_flip_v(true)
+		sprite.position = get_parent().position.x-10
+	pass
+
+
 func _physics_process(delta):
-	position.x = lerp(position.x, get_parent().position.x, 0.5)
-	position.y = lerp(position.y, get_parent().position.y, 0.5)
+	"""position.x = lerp(position.x, get_parent().position.x, 0.75)
+	position.y = lerp(position.y, get_parent().position.y, 0.75)"""
+	
 	var mouse_pos = get_global_mouse_position()
-	#if get_global_mouse_position().x < position.x:
-	#	sprite.flip_v = true
+	
 	
 	look_at(mouse_pos)
 	
