4179236740df528bd2b583b5325cbf2ae206a853
GSM429 <gavmontross@gmail.com>
2023-11-29 18:52:04 -0500

    Added Files to Main

--- Scripts/TestPlayer.gd
+++ Scripts/TestPlayer.gd
@@ -1,20 +1,23 @@
 extends CharacterBody2D
 
 const SPEED = 200
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -400
+const GRAVITY = 600
 
+var hasJumped = false
 
 func _physics_process(delta):
+
+	velocity.y += GRAVITY * delta
+
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y = 1 * SPEED
-	
-	if Input.is_action_just_pressed("ui_down"):
-		velocity.y = -1 * SPEED
+		hasJumped = true
+	if Input.is_action_just_pressed("ui_accept") and hasJumped == true:
+		velocity.y += -300
+		hasJumped = false
 	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
@@ -27,4 +30,9 @@ func _physics_process(delta):
 
 		
 	
-	
+func die():
+	queue_free()
+
+
+func _on_kill_area_body_entered(body):
+	die()
