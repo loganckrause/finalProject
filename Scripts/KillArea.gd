c314a721c75a5293f1d999bd109e184268054336
GSM429 <gavmontross@gmail.com>
2023-12-02 16:52:33 -0500

    Added Stuff

--- Scripts/KillArea.gd
+++ Scripts/KillArea.gd
@@ -1,5 +1,2 @@
 extends Area2D
 
-
-
-		
--- Scripts/TestPlayer.gd
+++ Scripts/TestPlayer.gd
@@ -1,38 +1,88 @@
 extends CharacterBody2D
 
-const SPEED = 200
-const JUMP_VELOCITY = -400
-const GRAVITY = 600
 
-var hasJumped = false
+const acceleration = 2000
+const deceleration = 1000
+
+var gravity = 980
+const maxSpeed = 200
+const JUMP_VELOCITY = -300
+var canDoubleJump = false
+
+const WALL_JUMP_PUSH = 175
+const WALL_SLIDE_FRICTION = 100
+var is_wall_sliding = false
+
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+
 
 func _physics_process(delta):
-
-	velocity.y += GRAVITY * delta
-
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		hasJumped = true
-	if Input.is_action_just_pressed("ui_accept") and hasJumped == true:
-		velocity.y += -300
-		hasJumped = false
+	# dir[0] = input_dir
+	# dir[1] = xInput
+	var inputDir = xinput()
 	
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	xaccelerate(inputDir, delta)
+	player_movement(velocity)
+	jump(delta)
+	wall_slide(delta)
 
+func player_movement(velocity):
 	move_and_slide()
-
-
-		
 	
-func die():
-	queue_free()
+func xaccelerate(direction, delta):
+	var xInput = direction * acceleration * delta
+	velocity.x += xInput
+	if abs(velocity.x) > maxSpeed:
+		velocity.x = direction * maxSpeed
+	
+	if direction == 0:
+		var friction = min(deceleration * delta, abs(velocity.x))
+		if velocity.x > 0:
+			velocity.x = max(0, velocity.x - friction)
+		elif velocity.x < 0:
+			velocity.x = min(0, velocity.x + friction)
 
+func xinput():
+	var inputDir = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	return inputDir
 
-func _on_kill_area_body_entered(body):
-	die()
+func jump(delta):
+	velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			canDoubleJump = true
+			velocity.y = JUMP_VELOCITY
+		if not is_on_floor():
+			if canDoubleJump and Input.is_action_just_pressed("jump"):
+				canDoubleJump = false
+				velocity.y = JUMP_VELOCITY
+			if is_on_wall() and Input.is_action_pressed("left"):
+				velocity.y = JUMP_VELOCITY 
+				velocity.x = WALL_JUMP_PUSH
+			if is_on_wall() and Input.is_action_pressed("right"):
+				velocity.y = JUMP_VELOCITY
+				velocity.x = -WALL_JUMP_PUSH
+
+func wall_slide(delta):
+	if is_on_wall() and not is_on_floor():
+		if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
+			is_wall_sliding = true
+		else:
+			is_wall_sliding = false
+	else:
+		is_wall_sliding = false
+	
+	if is_wall_sliding:
+		velocity.y += (WALL_SLIDE_FRICTION * delta)
+		velocity.y = min(velocity.y, WALL_SLIDE_FRICTION)
+		
+	pass
+
+func _input(event : InputEvent):
+	if(event.is_action_pressed("down") && is_on_floor()):
+		position.y += 1
